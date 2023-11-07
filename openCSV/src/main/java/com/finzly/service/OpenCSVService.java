b320ee79ebd07d0a1a74658deefda1db4afcfcdc
MeghaliLikhar <MeghaliLikhar@DESKTOP-6TLHQ09>
2023-11-07 18:00:07 +0530

    Update Script

--- openCSV/src/main/java/com/finzly/service/OpenCSVService.java
+++ openCSV/src/main/java/com/finzly/service/OpenCSVService.java
@@ -6,7 +6,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.List;
 
-import org.springframework.core.env.Environment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -46,7 +45,7 @@ public class OpenCSVService {
 		try (CSVReader reader = new CSVReaderBuilder(new InputStreamReader(inputStream, "UTF-8")).withSkipLines(0)
 				.build()) {
 
-			StringBuilder script = new StringBuilder();
+			StringBuilder insertscript = new StringBuilder();
 
 			String[] headerNames = reader.readNext();
 			String col1 = headerNames[0];
@@ -59,11 +58,11 @@ public class OpenCSVService {
 			for (String[] oneRecord : record) {
 				String value1 = oneRecord[0];
 				String value2 = oneRecord[1];
-				String query = "Insert into " + env + ".properties(" + col1 + "," + col2 + ") values('" + appName
+				String insertquery = "Insert into " + env + ".properties(" + col1 + "," + col2 + ") values('" + appName
 						+ "','" + value1 + "','" + value2 + "')";
-				script.append(query + "\n");
+				insertscript.append(insertquery + "\n");
 			}
-			return script.toString();
+			return insertscript.toString();
 
 		} catch (IOException | CsvException e) {
 			e.printStackTrace();
@@ -73,6 +72,57 @@ public class OpenCSVService {
 
 	private ResponseEntity<String> writeScriptToFile(String script) {
 		try (FileWriter writer = new FileWriter("src/main/resources/insertData1.sql")) {
+			writer.write(script);
+			return ResponseEntity.status(HttpStatus.OK).body("Script appended successfully");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body("Script not appended");
+	}
+
+//----------------------------------------------------------------------------------------------------------------------------------
+
+	public ResponseEntity<String> updateCsv(MultipartFile file) {
+		try {
+			String updatescript = csvParsingUpdate(file.getInputStream());
+			return writeUpdateScriptToFile(updatescript);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body("File Not Appended");
+	}
+
+	private String csvParsingUpdate(InputStream inputStream) {
+		try (CSVReader reader = new CSVReaderBuilder(new InputStreamReader(inputStream, "UTF-8")).withSkipLines(0)
+				.build()) {
+
+			StringBuilder updatescript = new StringBuilder();
+
+			String[] headerNames = reader.readNext();
+			String col1 = headerNames[0];
+			String col2 = headerNames[1];
+
+			String env = OpenCSVApplication.env;
+			// String appName = OpenCSVApplication.appName;
+
+			List<String[]> record = reader.readAll();
+			for (String[] oneRecord : record) {
+				String value1 = oneRecord[0];
+				String value2 = oneRecord[1];
+				String updatequery = "UPDATE " + env + ".properties SET " + col2 + " = '" + value2 + "' WHERE " + col1
+						+ " = '" + value1 + "'";
+				updatescript.append(updatequery + "\n");
+			}
+			return updatescript.toString();
+		} catch (IOException | CsvException e) {
+			e.printStackTrace();
+		}
+		return "Script not generated";
+	}
+
+	private ResponseEntity<String> writeUpdateScriptToFile(String script) {
+		try (FileWriter writer = new FileWriter("src/main/resources/updateData.sql")) {
 			writer.write(script);
 			return ResponseEntity.status(HttpStatus.OK).body("Script appended successfully");
 		} catch (IOException e) {
